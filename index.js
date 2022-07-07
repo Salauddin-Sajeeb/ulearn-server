970b4e95fd0d3fa025dced1e4acfc518f1310f4a
Salauddin Sajeebe <salauddin15-749@diu.edu.bd>
2022-07-07 10:59:36 +0600

    databse

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const port = process.env.PORT || 9000
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vewtb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fgcue.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 console.log(uri)
 app.get('/', (req, res) => {
